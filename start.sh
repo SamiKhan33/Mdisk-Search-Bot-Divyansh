9f8fa422e96603b55c68703bbccb831fccfe5985
SamiKhan33 <129081958+SamiKhan33@users.noreply.github.com>
2023-03-28 16:35:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/SamiKhan33/Mdisk-Search-Bot-Divyansh /MdiskWalaBot
+git clone https://github.com/SamiKhan33/Mdisk-Search-Bot-Divyansh /Mdisk-Search-Bot-Divyansh
 cd /Mdisk-Search-Bot-Divyansh
 pip3 install -r requirements.txt
 echo "Starting Bot...."
